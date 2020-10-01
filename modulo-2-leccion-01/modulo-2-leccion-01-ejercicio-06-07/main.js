d1f9ebfaf725990e06bda33ceaf8eecc2a4aee32
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-01 11:26:51 +0200

    modulo-2-ejercicio-08

--- modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-06-07/main.js
+++ modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-06-07/main.js
@@ -1,7 +1,8 @@
 "use strict";
 const containerElement = document.querySelector(".container");
-const content = "<h1>Lorem ipsum</h1><p>Lorem ipsum</p>";
-// const content = "<img scr="http://via.placeholder.com/350x150">";
+const content =
+  '<h1>Lorem ipsum</h1><p>Lorem ipsum</p><img scr="http://via.placeholder.com/350x150">';
+
 containerElement.innerHTML = content;
 const listElement = document.querySelector(".list");
 listElement.innerHTML = "<li>1</li><li>2</li><li>3</li>";
---
+++ modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-08/main.js
@@ -0,0 +1,4 @@
+"use strict";
+const buttonB = document.querySelector(".button-b");
+buttonB;
+buttonB.classList.add;
