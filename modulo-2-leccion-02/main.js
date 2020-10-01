9b510ff4f81a64ee9130617e2ba25a5113879683
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-01 23:05:44 +0200

    module-2-lesson-02

---
+++ modulo-2-leccion-02/main.js
@@ -0,0 +1,27 @@
+// Ejercicio 02
+const superWave = document.querySelector(".super-wave");
+superWave.classList.add("size");
+console.log("superWave");
+// Ejercicio 03
+const uno = "kiwis";
+const dos = "peras";
+const tres = "uvas";
+console.log(uno + dos + tres);
+const price = 4;
+const kilos = 0.5;
+console.log(price * kilos);
+// Ejercicio 05
+const day = "24";
+const year = "365";
+const age = "37";
+console.log(24 * 365 * 37);
+// Ejercicio 06-07
+const nameElement = "Lola";
+console.log(`Hola ${nameElement}, encantada de conocerte`);
+// typeof
+let text = "¿De qué tipo soy?";
+console.log(text + " es del tipo " + typeof text);
+text = 123;
+console.log("ahora " + text + " es del tipo " + typeof text);
+text = undefined;
+console.log("y ahora " + text + " es del tipo " + typeof text);
