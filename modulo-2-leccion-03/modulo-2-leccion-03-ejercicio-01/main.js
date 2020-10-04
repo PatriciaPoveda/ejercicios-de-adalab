e1a24555c6f21f0579a1466ba387d4b4301fc13f
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-04 22:54:14 +0200

    module-2-lesson-03

---
+++ modulo-2-leccion-03/modulo-2-leccion-03-ejercicio-01/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const Avatar = document.querySelector(".user__avatar");
+// avatar que eligió el usuario al registrarse
+let userAvatar = "http://www.fillmurray.com/300/300";
+const IsAvatar = userAvatar === Avatar;
+// avatar por defecto
+const DEFAULT_AVATAR = "http://placehold.it/300x300";
