d27ee454d82cfb0f593150a6c744e877d8260972
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-09 11:42:08 +0200

    module-1-lesson-07

---
+++ modulo-2-leccion-07/main.js
@@ -0,0 +1,17 @@
+"use strict";
+// EJERCICIO 01
+function workWithMovies(){
+  const films = ["Casablanca", "Titanic", "Dumbo"];
+  films.push("Los Pajaros")
+  // films.pop();
+  console.log(films);
+  films[1] = "Psicosis";
+  console.log(films);
+  films[2] = "Platoon";
+  console.log(films);
+}
+workWithMovies()
+// EJERCICIO 02
+// for (let i = 1; i < 11; i++) {
+//     console.log('Voy por la vuelta ' + i);
+//   }
