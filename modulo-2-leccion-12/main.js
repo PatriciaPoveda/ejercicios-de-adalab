5ba7ead817c3bcb870180949e21f0dc1b5844e14
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-20 11:51:30 +0200

    module-2-lesson-12

---
+++ modulo-2-leccion-12/main.js
@@ -0,0 +1,45 @@
+"use strict";
+// EJERCICIO 01
+const numbers = [1, 2, 3];
+
+for(let i = 0; i < numbers.length; i++){
+  const newItem = document.createElement('li');
+  const newContent = document.createTextNode(numbers[i]);
+  newItem.appendChild(newContent);
+  const items = document.querySelector('.items');
+  items.appendChild(newItem);
+}
+// EJERCICIO 03
+const peopleList = [
+  {name: "Patricia",
+lastName: "Poveda",
+telephone: "620620781"
+},
+  {name: "Edurne",
+lastName: "Vila",
+telephone: "607062812"
+},
+  {name: "Bruce",
+lastName: "Wayne",
+telephone: "555555555"
+}
+];
+
+console.log(peopleList[0].name);
+const selectEl = document.querySelector(".select");
+const inputList = document.querySelectorAll(".input");
+                                                               
+function fillForm(){
+  for (let i = 0; i < peopleList.length; i++) {
+    const element = peopleList[i];
+    // console.log(element);
+    console.log(selectEl.value);
+    if (selectEl.value === element.name){
+      document.querySelector("#name").value = element.name;
+      document.querySelector("#lastName").value = element.lastName;
+      document.querySelector("#telephone").value = element.telephone;
+    }    
+  }
+}
+
+selectEl.addEventListener("change", fillForm);
