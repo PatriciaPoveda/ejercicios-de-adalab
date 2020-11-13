d17faf3117fd5510314726d005a82fcc7fe409f8
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-11-13 11:31:42 +0100

    module-3-lesson-08

---
+++ modulo-3-leccion-08/src/components/Form.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+// import './';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.state = {
+      name: "Miguel",
+      description: "",
+    };
+  }
+  handleChange(event) {
+    this.setState({
+      name: event.currentTarget.value,
+    });
+  }
+  handleChange2(event) {
+    this.setState({
+      description: event.currentTarget.value,
+    });
+  }
+
+  render() {
+    console.log(this.state.description);
+    return (
+      <form>
+        <label htmlFor="name">Nombre</label>
+        <input
+          id="name"
+          type="text"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="text">Descripción</label>
+        <textarea
+          id="text"
+          type="text"
+          value={this.state.description}
+          onChange={this.handleChange2}
+        ></textarea>
+        <select>
+          <option value="Elige idioma">Elige idioma</option>
+          <option value="Español">Español</option>
+          <option value="Inglés">Inglés</option>
+          <option value="Portugués">Portugués</option>
+        </select>
+      </form>
+    );
+  }
+}
+export default Form;
