9e9cf846ac9474f491f2f6360f88e5ac0e10a5d0
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-12-14 18:14:48 +0100

    module-2-lesson-01-exercice-05

--- modulo-2/modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-01-03/main.js
+++ modulo-2/modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-01-03/main.js
@@ -1,9 +1,11 @@
 "use strict";
-debugger;
 document.querySelector("h1").innerHTML = "¡Hola mundo!";
+//Ejercicio 01
 document.querySelector("h1").innerHTML =
   "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente";
+//Ejercicio 02
 let myAddress = "Comandante fortea, 13";
 myAddress = "Calle Leganitos, 24";
+//Ejercicio 03
 const textElement = document.querySelector(".text");
 textElement.innerHTML = "Hola mundo";
--- modulo-2/modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-04-05/main.js
+++ modulo-2/modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-04-05/main.js
@@ -1,6 +1,10 @@
 "use strict";
+//Ejercicio 04
 const titleElement = document.querySelector(".title");
-// titleElement.innerHTML = "La Adalaber selecionada es: Celia";
-titleElement.innerHTML = titleElement.innerHTML + "Celia";
+const name1 = document.querySelector(".name1");
+const name2 = document.querySelector(".name2");
+// titleElement.innerHTML = titleElement.innerHTML + " " + name1.innerHTML;
+titleElement.innerHTML = titleElement.innerHTML + " " + name2.innerHTML;
+//Ejercicio 05
 const passwordElement = document.querySelector(".password");
-passwordElement.innerHTML = "**";
+passwordElement.innerHTML = "Mi contraseña es: **";
