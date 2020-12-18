36324d34080069975ef7744499a9ef7f6333800e
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-12-18 13:17:26 +0100

    module 2 lesson 01

--- modulo-2/modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-06-07/main.js
+++ modulo-2/modulo-2-leccion-01/modulo-2-leccion-01-ejercicio-06-07/main.js
@@ -1,8 +1,13 @@
 "use strict";
+
+// EJERCICIO 06
 const containerElement = document.querySelector(".container");
 const content =
-  '<h1>Lorem ipsum</h1><p>Lorem ipsum</p><img scr="http://via.placeholder.com/350x150">';
+  '<h1>Lorem ipsum</h1><img scr="http://via.placeholder.com/350x150" alt="placeholder"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>';
 
 containerElement.innerHTML = content;
+
+// EJERCICIO 07
+
 const listElement = document.querySelector(".list");
 listElement.innerHTML = "<li>1</li><li>2</li><li>3</li>";
