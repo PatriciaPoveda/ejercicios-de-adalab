41bc6d428ff90de53d28b8c82b639696f6959a47
PatriciaPoveda <patriciapovedaboto@gmail.com>
2021-02-24 18:03:22 +0100

    module 4 lesson 3.2

--- modulo-4/modulo-4-leccion-03/modulo-4-leccion-3.2-ejercicio-01/src/index.js
+++ modulo-4/modulo-4-leccion-03/modulo-4-leccion-3.2-ejercicio-01/src/index.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
 // create app server
@@ -9,13 +9,16 @@ app.listen(serverPort, () => {
 });
 
 // config express static server
-const staticServerPath = './public'; // relative to the root of the project
+const staticServerPath = "./public"; // relative to the root of the project
 app.use(express.static(staticServerPath));
 
 // not found error
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   // relative to this directory
-  const notFoundFileRelativePath = '../public/404-not-found.html';
-  const notFoundFileAbsolutePath = path.join(__dirname, notFoundFileRelativePath);
+  const notFoundFileRelativePath = "../public/404-not-found.html";
+  const notFoundFileAbsolutePath = path.join(
+    __dirname,
+    notFoundFileRelativePath
+  );
   res.status(404).sendFile(notFoundFileAbsolutePath);
 });
